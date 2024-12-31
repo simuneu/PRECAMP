5cdd7564769c8d4c175c7595fff98bf3c4c5c16f
simuneu <worksimu@gmail.com>
2024-12-31 23:50:00 +0900

    js

---
+++ class/03-javascript/연습-01-array.js
@@ -0,0 +1,22 @@
+let classmates = ["철수","영희","재석"]
+// undefined
+classmates
+// (3) ['철수', '영희', '재석']
+classmates[0]
+// '철수'
+classmates[1]
+// '영희'
+classmates[2]
+// '재석'
+classmates.includes("철수")
+// true
+classmates.length
+// 3
+classmates.push("맹구")
+// 4
+classmates.includes("맹구")
+// true
+classmates.pop(3)
+// '맹구'
+classmates.length
+// 3
