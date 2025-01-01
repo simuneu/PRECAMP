c01b8589029532b2acd20f5fa7e4634d686acdab
simuneu <worksimu@gmail.com>
2025-01-01 23:44:47 +0900

    js object

---
+++ class/03-javascript/02-object-array.js
@@ -0,0 +1,25 @@
+const fruits=[
+    {number:1, title:"딸기"},
+     {number:2, title:"사과"},
+     {number:3, title:"배"},
+     {number:4, title:"포도"},
+     {number:5, title:"감"},
+     {number:6, title:"귤"},
+     {number:7, title:"용과"},
+     {number:8, title:"리치"},
+     {number:9, title:"수박"},
+     {number:10, title:"복숭아"}
+]
+// undefined
+fruits[0].number+ fruits[0].title
+// '1딸기'
+fruits[0].number+ " "+fruits[0].title
+// '1 딸기'
+fruits[1].number+ " "+fruits[1].title
+// '2 사과'
+fruits[2].number+ " "+fruits[2].title
+// '3 배'
+fruits[1].number+ " "+fruits[1].title
+// '2 사과'
+fruits[3].number+ " "+fruits[3].title
+// '4 포도'
---
+++ class/03-javascript/연습-04-object-in-array.js
@@ -0,0 +1,14 @@
+let classmates = [
+    { name:"철수", age:13, school:"다람쥐초등학교"},
+    {name:"영희", age:8, shcool:"공룡초등학교"},
+    {name:"훈이",age:9, school:"거북초등학교"}
+]
+// undefined
+classmates.length
+// 3
+length
+// 0
+classmates[0].school
+// '다람쥐초등학교'
+classmates[2].age
+// 9
