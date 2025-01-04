fcd0bf6ebb28b51a467e8cb053b3a92d33053871
simuneu <worksimu@gmail.com>
2025-01-04 23:34:23 +0900

    dom

---
+++ class/03-javascript/04-document.js
@@ -0,0 +1,5 @@
+function greeting() {
+    document.getElementById("target").innerText ="world"
+    // document.getElementById("input").value ="world"
+
+}
---
+++ class/03-javascript/연습-07-math-random.js
@@ -0,0 +1,25 @@
+Math.floor(Math.random() *1000000)
+// 784969
+
+String(Math.floor(Math.random() *1000000) )
+// '468733'
+String(Math.floor(Math.random() *1000000) )
+// '977129'
+String(Math.floor(Math.random() *1000000) )
+// '22949'
+
+String(Math.floor(Math.random() *1000000) ).padStart(6,"0")
+// '869382'
+String(Math.floor(Math.random() *1000000) ).padStart(6,"0")
+// '157782'
+String(Math.floor(Math.random() *1000000) ).padStart(6,"0")
+// '066521'
+
+let result =     String(Math.floor(Math.random() *1000000) ).padStart(6,"0")
+// undefined
+result
+// '190038'
+result
+// '190038'
+result
+// '190038'
