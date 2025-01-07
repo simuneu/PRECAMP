96519130f7db51947a997a92d1889be6ea42a6ab
simuneu <worksimu@gmail.com>
2025-01-07 23:49:32 +0900

    focus, signup

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,19 +1,35 @@
+let isStarted=false;
+
 let auth=(target)=> {
-    const token = String (Math.floor(Math.random()*1000000)).padStart(6,"0")
-     document.getElementById("target").innerText = token
-     document.getElementById("target").style.color = "#"+token;
 
-     
-    let time= 5
+    if(isStarted === false) {
+        //타이머 작동 중이지 않을 때
+        isStarted=true
+        document.getElementById("button").disabled = false
+        const token = String (Math.floor(Math.random()*1000000)).padStart(6,"0")
+         document.getElementById("target").innerText = token
+         document.getElementById("target").style.color = "#"+token;
+    
+         
+        let time= 10
+        let timer
+    
+        timer = setInterval(function () {
 
-    setInterval(function () {
-        if(time>=0) {
-            let min = Math.floor(time/60)
-            let sec = String(time%60).padStart(2,"0")
-            document.getElementById("timer").innerText = min+":"+sec
-            time = time-1
-        }else{
-            document.getElementById("button").disabled = true;
-        }
-    },1000)
+            if(time>=0) {
+                let min = Math.floor(time/60)
+                let sec = String(time%60).padStart(2,"0")
+                document.getElementById("timer").innerText = min+":"+sec
+                time = time-1
+            }else{
+                document.getElementById("button").disabled = true;
+                isStarted =false
+                clearInterval(timer)
+            }
+
+        },1000)
+    }else{
+        //타이머 작동 중
+    }
+
  }
